f2b85fc93a8bebb2ae2d358b498111715fcec296
JeongSunghan <jsh000311@gmail.com>
2024-02-05 12:50:41 +0900

    Sql

--- src/Mysql2/ERD/sec01_basic/Ex03_UpdateWithParameter.java
+++ src/Mysql2/ERD/sec01_basic/Ex03_UpdateWithParameter.java
@@ -7,25 +7,24 @@ import java.sql.PreparedStatement;
 public class Ex03_UpdateWithParameter {
 
 	public static void main(String[] args) {
-		update(2340, "수원" , 1200000);
-		
+		update(2340, "수원", 1200000);
 	}
 
-	static void update (int id, String name, int population) {
+	static void update(int id, String name, int population) {
 		try {
 			Connection conn = DriverManager.getConnection(
-					"jdbc:mysql://localhost:3306/world", "hmuser", "hmpass"	);
-			String sql = "select * from city where countrycode=? limit ?";
-			
-			//parameter 세팅
+					"jdbc:mysql://localhost:3306/world", "hmuser", "hmpass");
+			String sql = "update city set name=?, population=? where id=?";
+			// Parameter setting
 			PreparedStatement pstmt = conn.prepareStatement(sql);
 			pstmt.setString(1, name);
 			pstmt.setInt(2, population);
 			pstmt.setInt(3, id);
 			
-			//Update 실행 - 반환값이 없는 쿼리
-			pstmt.executeUpdate();	// Insert, Delete query도 이렇게 사용
-			pstmt.close(); conn.close();			
+			// Update 실행 - 반환값이 없는 쿼리
+			pstmt.executeUpdate();			// Insert, Delete query도 이렇게 사용
+			
+			pstmt.close(); conn.close();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
